ae88f53aa26ec479170b9c06355d221efb86a7a9
Akhator Itua <iakhator@Iakhator.local>
2017-12-17 09:54:10 +0100

    style theme for ayu-mirage

---
+++ package.js
@@ -0,0 +1,58 @@
+{
+  "_args": [
+    [
+      {
+        "raw": "/private/var/folders/qr/fll613g57xb14_nndjp3y0_00000gn/T/d-1171116-605-1j388qp.hfus5j0pb9/package.tgz",
+        "scope": null,
+        "escapedName": null,
+        "name": null,
+        "rawSpec": "/private/var/folders/qr/fll613g57xb14_nndjp3y0_00000gn/T/d-1171116-605-1j388qp.hfus5j0pb9/package.tgz",
+        "spec": "/private/var/folders/qr/fll613g57xb14_nndjp3y0_00000gn/T/d-1171116-605-1j388qp.hfus5j0pb9/package.tgz",
+        "type": "local"
+      },
+      "/private/var/folders/qr/fll613g57xb14_nndjp3y0_00000gn/T/apm-install-dir-1171116-605-vmrnu7.mcpduz0k9"
+    ]
+  ],
+  "_from": "../d-1171116-605-1j388qp.hfus5j0pb9/package.tgz",
+  "_id": "ayu-mirage@0.1.0",
+  "_inCache": true,
+  "_location": "/ayu-mirage-atom-syntax",
+  "_phantomChildren": {},
+  "_requested": {
+    "raw": "/private/var/folders/qr/fll613g57xb14_nndjp3y0_00000gn/T/d-1171116-605-1j388qp.hfus5j0pb9/package.tgz",
+    "scope": null,
+    "escapedName": null,
+    "name": null,
+    "rawSpec": "/private/var/folders/qr/fll613g57xb14_nndjp3y0_00000gn/T/d-1171116-605-1j388qp.hfus5j0pb9/package.tgz",
+    "spec": "/private/var/folders/qr/fll613g57xb14_nndjp3y0_00000gn/T/d-1171116-605-1j388qp.hfus5j0pb9/package.tgz",
+    "type": "local"
+  },
+  "_requiredBy": [
+    "#USER"
+  ],
+  "_resolved": "file:../d-1171116-605-1j388qp.hfus5j0pb9/package.tgz",
+  "_shasum": "c9dd0deeb9896d1567f6983e07a1e0a984787047",
+  "_shrinkwrap": null,
+  "_spec": "/private/var/folders/qr/fll613g57xb14_nndjp3y0_00000gn/T/d-1171116-605-1j388qp.hfus5j0pb9/package.tgz",
+  "_where": "/private/var/folders/qr/fll613g57xb14_nndjp3y0_00000gn/T/apm-install-dir-1171116-605-vmrnu7.mcpduz0k9",
+  "bugs": {
+    "url": "https://github.com/iakhator/ayu-mirage-atom-syntax/issues"
+  },
+  "dependencies": {},
+  "description": "Converted version of the Ayu syntax theme for Sublime text",
+  "devDependencies": {},
+  "engines": {
+    "atom": ">=1.0.0 <2.0.0"
+  },
+  "keywords": [
+    "syntax",
+    "theme"
+  ],
+  "license": "MIT",
+  "name": "ayu",
+  "optionalDependencies": {},
+  "readme": "# ayu-mirage theme\n\nA short description of your theme.\n\n![A screenshot of your theme](https://f.cloud.github.com/assets/69169/2289498/4c3cb0ec-a009-11e3-8dbd-077ee11741e5.gif)\n",
+  "readmeFilename": "README.md",
+  "theme": "syntax",
+  "version": "0.1.0"
+}
